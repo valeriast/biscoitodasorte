6a84d6f615d93e70924ecbe0b870a3d8ae26e3ac
Valeria <valeria.s@outlook.com.br>
2022-11-02 19:59:30 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+function App(){
+  const [img, setImg] = useState(require('./src/biscoito.png'));
+
+  return(
+    <View style={styles.container}>
+      <Image 
+        source={img}
+        style={styles.img}/>
+
+      <Text style={styles.textofrase}> " Frase biscoito "</Text>
+
+      <TouchableOpacity style={styles.botao}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.botao, {marginTop: 15, borderColor: '#121212'  }]}>
+        <View style={styles.btnArea}>
+          <Text style={[styles.btnTexto, {color: '#121212'}]}>Reiniciar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  img:{
+    width: 230,
+    height: 230
+  },
+  textofrase:{
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao:{
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25
+  },
+  btnArea:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto:{
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  }
+})
+
+export default App;
---
+++ README.md
@@ -0,0 +1,6 @@
+#Biscoito Da Sorte (fortune cookie) / JS - React Native Learning
+
+It`s an app to retrieve fortune cookie phrases.
+
+This app is created along with a JS/React Native course from "Sujeito Programador".
+ 
